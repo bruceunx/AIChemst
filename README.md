07c598c1f115e6aea3805f4b2259c1dc4d70bfbb
bruce <cnhxunoo@gmail.com>
2023-11-09 19:23:27 +0800

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Retrosynthesis Frontend
 
 - use nextjs and reactflow to handle retrosynthesis frontend
+
+### Screenshots
+
+### AI design synthesis routes and conditions
+
+![](./sample1.png)
+![](./sample2.png)
+![](./sample3.png)
--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,20 +1,30 @@
-import { Flex, Link } from '@radix-ui/themes'
+import { Flex, Link, Text } from '@radix-ui/themes'
 
 export default function Nav() {
-	return (
-		<Flex
-			className='h-screen w-28'
-			mt='8'
-			p='2'
-			pt='5'
-			position='fixed'
-			direction='column'
-			align='center'
-			gap='2'
-			style={{ backgroundColor: 'var(--gray-a6)' }}
-		>
-			<Link>当前查询</Link>
-			<Link>历史查询</Link>
-		</Flex>
-	)
+  return (
+    <Flex
+      className='h-[95vh] w-28'
+      mt='8'
+      p='2'
+      pt='5'
+      position='fixed'
+      direction='column'
+      align='center'
+      justify='between'
+      style={{ backgroundColor: 'var(--gray-a6)' }}
+    >
+      <Flex direction='column' gap='2'>
+        <Link>当前查询</Link>
+        <Link>历史查询</Link>
+      </Flex>
+      <Flex direction='column'>
+        <Text size='1' color='ruby'>
+          wx: labnetworks
+        </Text>
+        <Text size='1' color='blue'>
+          辅助功能开发中
+        </Text>
+      </Flex>
+    </Flex>
+  )
 }
